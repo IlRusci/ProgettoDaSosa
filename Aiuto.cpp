de0acfef04e15f9785f0bce73e6909a8369b7cd7
IlRusci <79568702+IlRusci@users.noreply.github.com>
2021-05-20 21:58:08 +0200

    Update Aiuto.cpp

--- Aiuto.cpp
+++ Aiuto.cpp
@@ -1,10 +1,10 @@
 #include "Aiuto.h" 
-
+system("Color E4");
 Aiuto :: Aiuto(){
 	for (int i=0; i<3; i++)
 		aiuti_utilizzati[i] = 0;
 }
-system("Color E4");
+
 bool Aiuto :: usa_aiuto(ListaDomande l, Domanda d, int i){
 	int x;
 	if (i<1 || i>3){
